1525ce6c7cc10ff444a853c47b21713d0a4bccec
dev12 <46024391+Aidann32@users.noreply.github.com>
2023-01-25 21:04:01 +0500

    Created main page

--- config/settings.py
+++ config/settings.py
@@ -128,7 +128,6 @@ STATIC_ROOT = ''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'), 
 ]
-print(STATICFILES_DIRS)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
